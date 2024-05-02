4d2684eaa6399fccaf809dd2e60cd23f3d3a7985
jbara <medanisjbara@proton.me>
2024-05-02 05:18:44 +0100

    implement basic portal messageLoop
    this commit doesn't include all handlers

---
+++ msgconv/from-matrix.go
@@ -0,0 +1,42 @@
+package msgconv
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"golang.org/x/exp/constraints"
+)
+
+var (
+	ErrUnsupportedMsgType  = errors.New("unsupported msgtype")
+	ErrMediaDownloadFailed = errors.New("failed to download media")
+	ErrMediaDecryptFailed  = errors.New("failed to decrypt media")
+	ErrMediaConvertFailed  = errors.New("failed to convert")
+	ErrMediaUploadFailed   = errors.New("failed to upload media")
+	ErrInvalidGeoURI       = errors.New("invalid `geo:` URI in message")
+)
+
+func maybeInt[T constraints.Integer](v T) *T {
+	if v == 0 {
+		return nil
+	}
+	return &v
+}
+
+func parseGeoURI(uri string) (lat, long string, err error) {
+	if !strings.HasPrefix(uri, "geo:") {
+		err = fmt.Errorf("uri doesn't have geo: prefix")
+		return
+	}
+	// Remove geo: prefix and anything after ;
+	coordinates := strings.Split(strings.TrimPrefix(uri, "geo:"), ";")[0]
+	splitCoordinates := strings.Split(coordinates, ",")
+	if len(splitCoordinates) != 2 {
+		err = fmt.Errorf("didn't find exactly two numbers separated by a comma")
+	} else {
+		lat = splitCoordinates[0]
+		long = splitCoordinates[1]
+	}
+	return
+}
--- portal.go
+++ portal.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"strconv"
+    "time"
+    "fmt"
 
 	"github.com/rs/zerolog"
 
+	"maunium.net/go/mautrix"
 	"maunium.net/go/mautrix/appservice"
 	"maunium.net/go/mautrix/bridge"
 	"maunium.net/go/mautrix/bridge/bridgeconfig"
@@ -115,6 +118,209 @@ func (portal *Portal) HasRelaybot() bool {
 
 func (portal *Portal) getBridgeInfo() (string, string) {
 	return "", ""
+}
+
+func (portal *Portal) messageLoop() {
+	for {
+		select {
+		case msg := <-portal.matrixMessages:
+			portal.handleMatrixMessages(msg)
+		case msg := <-portal.emailMessages:
+			portal.handleEmailMessage(msg)
+		}
+	}
+}
+
+func (portal *Portal) handleMatrixMessages(msg portalMatrixMessage) {
+	log := portal.log.With().
+		Str("action", "handle matrix event").
+		Stringer("event_id", msg.evt.ID).
+		Str("event_type", msg.evt.Type.String()).
+		Logger()
+	ctx := log.WithContext(context.TODO())
+
+	switch msg.evt.Type {
+	case event.EventMessage, event.EventSticker:
+		portal.handleMatrixMessage(ctx, msg.user, msg.evt)
+	default:
+		log.Warn().Str("type", msg.evt.Type.Type).Msg("Unhandled matrix message type")
+	}
+}
+
+func (portal *Portal) handleMatrixMessage(ctx context.Context, sender *User, evt *event.Event) {
+	log := zerolog.Ctx(ctx)
+	evtTS := time.UnixMilli(evt.Timestamp)
+	timings := messageTimings{
+		initReceive:  evt.Mautrix.ReceivedAt.Sub(evtTS),
+		decrypt:      evt.Mautrix.DecryptionDuration,
+		totalReceive: time.Since(evtTS),
+	}
+	implicitRRStart := time.Now()
+	timings.implicitRR = time.Since(implicitRRStart)
+	start := time.Now()
+
+	messageAge := timings.totalReceive
+	ms := metricSender{portal: portal, timings: &timings, ctx: ctx}
+	log.Debug().
+		Stringer("sender", evt.Sender).
+		Dur("age", messageAge).
+		Msg("Received message")
+
+	errorAfter := portal.bridge.Config.Bridge.MessageHandlingTimeout.ErrorAfter
+	deadline := portal.bridge.Config.Bridge.MessageHandlingTimeout.Deadline
+	isScheduled, _ := evt.Content.Raw["com.beeper.scheduled"].(bool)
+	if isScheduled {
+		log.Debug().Msg("Message is a scheduled message, extending handling timeouts")
+		errorAfter *= 10
+		deadline *= 10
+	}
+
+	if errorAfter > 0 {
+		remainingTime := errorAfter - messageAge
+		if remainingTime < 0 {
+			go ms.sendMessageMetrics(evt, errTimeoutBeforeHandling, "Timeout handling", true)
+			return
+		} else if remainingTime < 1*time.Second {
+			log.Warn().
+				Dur("remaining_time", remainingTime).
+				Dur("max_timeout", errorAfter).
+				Msg("Message was delayed before reaching the bridge")
+		}
+		go func() {
+			time.Sleep(remainingTime)
+			ms.sendMessageMetrics(evt, errMessageTakingLong, "Timeout handling", false)
+		}()
+	}
+
+	if deadline > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, deadline)
+		defer cancel()
+	}
+
+	timings.preproc = time.Since(start)
+	start = time.Now()
+
+	content, ok := evt.Content.Parsed.(*event.MessageEventContent)
+	if !ok {
+		log.Error().Type("content_type", content).Msg("Unexpected parsed content type")
+		go ms.sendMessageMetrics(evt, fmt.Errorf("%w %T", errUnexpectedParsedContentType, evt.Content.Parsed), "Error converting", true)
+		return
+	}
+
+	realSenderMXID := sender.MXID
+	isRelay := false
+	if !sender.IsLoggedIn() {
+		sender = portal.GetRelayUser()
+		if sender == nil {
+			go ms.sendMessageMetrics(evt, errUserNotLoggedIn, "Ignoring", true)
+			return
+		} else if !sender.IsLoggedIn() {
+			go ms.sendMessageMetrics(evt, errRelaybotNotLoggedIn, "Ignoring", true)
+			return
+		}
+		isRelay = true
+	}
+
+	var editTargetMsg *database.Message
+	if editTarget := content.RelatesTo.GetReplaceID(); editTarget != "" {
+		var err error
+		editTargetMsg, err = portal.bridge.DB.Message.GetByMXID(ctx, editTarget)
+		if err != nil {
+			log.Err(err).Stringer("edit_target_mxid", editTarget).Msg("Failed to get edit target message")
+			go ms.sendMessageMetrics(evt, errFailedToGetEditTarget, "Error converting", true)
+			return
+		} else if editTargetMsg == nil {
+			log.Err(err).Stringer("edit_target_mxid", editTarget).Msg("Edit target message not found")
+			go ms.sendMessageMetrics(evt, errEditUnknownTarget, "Error converting", true)
+			return
+		} else if editTargetMsg.Sender != sender.EmailAddress {
+			go ms.sendMessageMetrics(evt, errEditDifferentSender, "Error converting", true)
+			return
+		}
+		if content.NewContent != nil {
+			content = content.NewContent
+			evt.Content.Parsed = content
+		}
+	}
+
+	relaybotFormatted := isRelay && portal.addRelaybotFormat(ctx, realSenderMXID, evt, content)
+	if content.MsgType == event.MsgNotice && !portal.bridge.Config.Bridge.BridgeNotices {
+		go ms.sendMessageMetrics(evt, errMNoticeDisabled, "Error converting", true)
+		return
+	}
+	ctx = context.WithValue(ctx, msgconvContextKeyClient, sender.Client)
+	msg, err := portal.MsgConv.ToEmail(ctx, evt, content, relaybotFormatted)
+	if err != nil {
+		log.Err(err).Msg("Failed to convert message")
+		go ms.sendMessageMetrics(evt, err, "Error converting", true)
+		return
+	}
+
+	timings.convert = time.Since(start)
+	start = time.Now()
+
+	err = portal.sendEmailMessage(ctx, msg, sender, evt.ID)
+
+	timings.totalSend = time.Since(start)
+	go ms.sendMessageMetrics(evt, err, "Error sending", true)
+	if err == nil {
+		if editTargetMsg != nil {
+			err = editTargetMsg.SetTimestamp(ctx, msg.GetTimestamp())
+			if err != nil {
+				log.Err(err).Msg("Failed to update message timestamp in database after editing")
+			}
+		} else {
+			portal.storeMessageInDB(ctx, evt.ID, sender.EmailAddress, msg.GetTimestamp(), 0)
+			if portal.ExpirationTime > 0 {
+				portal.addDisappearingMessage(ctx, evt.ID, uint32(portal.ExpirationTime), true)
+			}
+		}
+	}
+}
+
+// FIXME: delete this
+type DataMessage string
+
+func (portal *Portal) handleEmailMessage(portalMessage portalEmailMessage) {
+	sender := portal.bridge.GetPuppetByEmailAddress(portalMessage.evt.Info.Sender)
+	if sender == nil {
+		portal.log.Warn().
+			Stringer("sender_id", portalMessage.evt.Info.Sender).
+			Msg("Couldn't get puppet for message")
+		return
+	}
+	var msgType string
+	var timestamp uint64
+	switch typedEvt := portalMessage.evt.Event.(type) {
+    // FIXME
+	case *DataMessage:
+		msgType = "data"
+		timestamp = typedEvt.GetTimestamp()
+		portal.handleEmailDataMessage(portalMessage.user, sender, typedEvt)
+	default:
+		portal.log.Error().
+			Type("data_type", typedEvt).
+			Msg("Invalid inner event type inside ChatEvent")
+	}
+	portal.bridge.Metrics.TrackEmailMessage(time.UnixMilli(int64(timestamp)), msgType)
+}
+
+func (portal *Portal) sendMainIntentMessage(ctx context.Context, content *event.MessageEventContent) (*mautrix.RespSendEvent, error) {
+	return portal.sendMatrixEvent(ctx, portal.MainIntent(), event.EventMessage, content, nil, 0)
+}
+
+func (portal *Portal) getBridgeInfoStateKey() string {
+	return fmt.Sprintf("net.maunium.mybridge://bridge/%s", portal.ThreadID)
+}
+
+func (portal *Portal) GetRelayUser() *User {
+	if !portal.HasRelaybot() {
+		return nil
+	} else if portal.relayUser == nil {
+		portal.relayUser = portal.bridge.GetUserByMXID(portal.RelayUserID)
+	}
+	return portal.relayUser
 }
 
 // Bridge stuff related to Portals
