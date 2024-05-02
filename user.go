03509043e661036fe88bceb18b58239cdf64c742
jbara <medanisjbara@proton.me>
2024-05-02 04:19:05 +0100

    implement user specific methods

--- user.go
+++ user.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rs/zerolog"
 	"maunium.net/go/mautrix/bridge"
 	"maunium.net/go/mautrix/bridge/bridgeconfig"
+	"maunium.net/go/mautrix/id"
 )
 
 type User struct {
@@ -28,4 +29,60 @@ type User struct {
 
 	spaceMembershipChecked bool
 	spaceCreateLock        sync.Mutex
+}
+
+func (user *User) GetIDoublePuppet() bridge.DoublePuppet {
+	p := user.bridge.GetPuppetByCustomMXID(user.MXID)
+	if p == nil || p.CustomIntent() == nil {
+		return nil
+	}
+	return p
+}
+
+func (user *User) GetIGhost() bridge.Ghost {
+	p := user.bridge.GetPuppetByEmailAddress(user.EmailAddress)
+	if p == nil {
+		return nil
+	}
+	return p
+}
+
+func (user *User) GetMXID() id.UserID {
+	return user.MXID
+}
+
+func (user *User) GetManagementRoomID() id.RoomID {
+	return user.ManagementRoom
+}
+
+func (user *User) GetPermissionLevel() bridgeconfig.PermissionLevel {
+	return user.PermissionLevel
+}
+
+func (user *User) IsLoggedIn() bool {
+	user.Lock()
+	defer user.Unlock()
+
+	return user.Client != nil && user.Client.IsLoggedIn()
+}
+
+func (user *User) SetManagementRoom(roomID id.RoomID) {
+	user.bridge.managementRoomsLock.Lock()
+	defer user.bridge.managementRoomsLock.Unlock()
+
+	existing, ok := user.bridge.managementRooms[roomID]
+	if ok {
+		existing.ManagementRoom = ""
+		err := existing.Update(context.TODO())
+		if err != nil {
+			existing.log.Err(err).Msg("Failed to update user when removing management room")
+		}
+	}
+
+	user.ManagementRoom = roomID
+	user.bridge.managementRooms[user.ManagementRoom] = user
+	err := user.Update(context.TODO())
+	if err != nil {
+		user.log.Error().Err(err).Msg("Error setting management room")
+	}
 }
